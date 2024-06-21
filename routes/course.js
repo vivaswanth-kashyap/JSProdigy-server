4fe1ce969a7fdf5a8192b87a209ea5fa4da3aeae
Vivaswanth <kashyap.vivaswanth@gmail.com>
2024-06-21 23:10:16 +0530

    course route

---
+++ routes/course.js
@@ -0,0 +1,11 @@
+import express from "express";
+import xss from "xss";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+	return res.status(200).json({
+		title: "Full Stack Web development with javascript",
+		description: "This course is extremely comprehensive",
+	});
+});
