1ee25f4ac1add8693da580cdf7a9ea487665be4c
Vivaswanth <kashyap.vivaswanth@gmail.com>
2024-06-21 12:33:22 +0530

    atlas added

--- config/settings.js
+++ config/settings.js
@@ -1,4 +1,5 @@
 export const mongoConfig = {
-	serverUrl: "mongodb://0.0.0.0:27017/",
+	serverUrl:
+		"mongodb+srv://vivaswanth3:Y6Md8Pn8YsoXSLkE@cluster0.ltdhtfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 	database: "jsProdigy",
 };
