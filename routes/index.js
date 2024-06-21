bc086e440fb00fea56a850459eff3c130c64129b
Vivaswanth <kashyap.vivaswanth@gmail.com>
2024-06-21 23:13:52 +0530

    course route to index

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ import userRoutes from "./users.js";
 import videoRoutes from "./videos.js";
 import doubtRoutes from "./doubts.js";
 import homePageRoutes from "./home.js";
+import courseRoutes from "./course.js";
 
 const constructorMethod = (app) => {
 	app.use("/", homePageRoutes);
 	app.use("/users", userRoutes);
+	app.use("/course", courseRoutes);
 	app.use("/videos", videoRoutes);
 	app.use("/doubts", doubtRoutes);
 
